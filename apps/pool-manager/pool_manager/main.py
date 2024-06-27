10433e136ce75be3c62315d60f9fceac989aff9b
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-06-27 22:54:21 +0100

    add skeleton app

---
+++ apps/pool-manager/pool_manager/main.py
@@ -0,0 +1,9 @@
+from aws_lambda_powertools import Logger, Tracer
+
+logger = Logger()
+tracer = Tracer()
+
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context(log_event=True)
+def handler(_event, _context):
+    pass
