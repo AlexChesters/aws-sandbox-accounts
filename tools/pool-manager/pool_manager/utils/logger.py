0c37bac9bd8fba25adce6351d9c51d2e923eef46
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-06-28 14:25:01 +0100

    swap

--- tools/pool-manager/pool_manager/utils/logger.py
+++ tools/pool-manager/pool_manager/utils/logger.py
@@ -7,7 +7,7 @@ class Logger:
         return ", ".join(args)
 
     def plain(self, *args):
-        console.log(self.__format_msg(*args))
+        console.print(self.__format_msg(*args))
 
     def error(self, *args):
         console.print(f"[red][ERROR][/red] - {self.__format_msg(*args)}")
