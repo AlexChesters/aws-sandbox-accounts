c5d259df9554e110347fae98f15ff615dae9455c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-07-04 10:36:35 +0100

    serialise lists

--- apps/account-manager-db-client/account_manager_db_client/utils/db.py
+++ apps/account-manager-db-client/account_manager_db_client/utils/db.py
@@ -2,5 +2,8 @@ from boto3.dynamodb.types import TypeSerializer
 
 serialiser = TypeSerializer()
 
-def serialise(obj):
-    return {k: serialiser.serialize(v) for k,v in obj.items()}
+def serialise(i):
+    if isinstance(i, dict):
+        return {k: serialiser.serialize(v) for k,v in i.items()}
+    elif isinstance(i, list):
+        return [serialiser.serialize(v) for v in i]
