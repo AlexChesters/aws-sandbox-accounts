a3b1994723e9b882636a060ba548813082af18e3
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-07-12 11:45:53 +0100

    return value

--- apps/auth-client/auth_client/actions/create_lease.py
+++ apps/auth-client/auth_client/actions/create_lease.py
@@ -3,13 +3,21 @@ from aws_lambda_powertools import Logger
 logger = Logger()
 
 def create_lease(event, sso_client):
+    account_id = event["account"]["account_id"]
+    user_id = event["user_id"]
+
     response = sso_client.create_account_assignment(
         InstanceArn="arn:aws:sso:::instance/ssoins-680483867be3e0a4",
         PermissionSetArn="arn:aws:sso:::permissionSet/ssoins-680483867be3e0a4/ps-2fe74c0708584223",
-        PrincipalId=event["user_id"],
+        PrincipalId=user_id,
         PrincipalType="USER",
-        TargetId=event["account"]["account_id"],
+        TargetId=account_id,
         TargetType="AWS_ACCOUNT"
     )
 
     logger.info(response)
+
+    return {
+        "account_id": account_id,
+        "user_id": user_id
+    }
