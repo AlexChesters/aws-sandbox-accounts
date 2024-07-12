70a3e07c421e8be35266b35c7cb455fe437c0674
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-07-12 14:44:44 +0100

    remove

---
+++ apps/auth-client/auth_client/actions/remove_lease.py
@@ -0,0 +1,24 @@
+from aws_lambda_powertools import Logger
+
+logger = Logger()
+
+def remove_lease(event, sso_client):
+    account_id = event["account"]["account_id"]
+    user_id = event["user_id"]
+    lease_id = event["lease_id"]
+
+    response = sso_client.delete_account_assignment(
+        InstanceArn="arn:aws:sso:::instance/ssoins-680483867be3e0a4",
+        PermissionSetArn="arn:aws:sso:::permissionSet/ssoins-680483867be3e0a4/ps-2fe74c0708584223",
+        PrincipalId=user_id,
+        PrincipalType="USER",
+        TargetId=account_id,
+        TargetType="AWS_ACCOUNT"
+    )
+
+    logger.info(response)
+
+    return {
+        "account_id": account_id,
+        "lease_id": lease_id
+    }
