2d6c3e71b51b8498ef173140b2c6584c844e4b68
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-07-16 11:34:39 +0100

    pull duration to step fn

--- apps/auth-client/auth_client/actions/create_lease.py
+++ apps/auth-client/auth_client/actions/create_lease.py
@@ -4,10 +4,27 @@ from aws_lambda_powertools import Logger
 
 logger = Logger()
 
+# map of duration string: minutes
+valid_durations = {
+    "5m": 5,
+    "30m": 30,
+    "1h": 60,
+    "3h": 180,
+    "6h": 360,
+    "12h": 720,
+    "24h": 1440
+}
+
 def create_lease(event, sso_client):
     params = event["params"]
     account_id = params["account"]["account_id"]
     user_id = params["user_id"]
+    duration_param = params["duration"]
+
+    duration = valid_durations.get(duration_param, None)
+
+    if not duration:
+        raise ValueError(f"{duration_param} is not a valid duration, valid durations: {valid_durations.keys()}")
 
     response = sso_client.create_account_assignment(
         InstanceArn="arn:aws:sso:::instance/ssoins-680483867be3e0a4",
@@ -20,11 +37,8 @@ def create_lease(event, sso_client):
 
     logger.info(response)
 
-    # low limit for testing
-    one_minute_from_now = datetime.now() + timedelta(minutes=1)
-
     return {
         "account_id": account_id,
         "user_id": user_id,
-        "expires": one_minute_from_now.isoformat()
+        "expires": datetime.now() + timedelta(minutes=duration).isoformat()
     }
