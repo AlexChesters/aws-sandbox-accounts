33fa1bd1456fd85aa7f38314fac252b292cd1556
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-12-20 12:23:43 +0000

    formatting

--- apps/interface/leases/views.py
+++ apps/interface/leases/views.py
@@ -1,8 +1,10 @@
 import logging
+from datetime import datetime, timezone
 
 from django.http import HttpRequest
 from django.shortcuts import render
 import boto3
+import humanize
 
 from leases.utils.deserialise import deserialise
 from leases.utils.get_user_details import get_user_details
@@ -32,8 +34,17 @@ def index(request: HttpRequest):
 
         lease_data = lease_raw_data
 
-        lease_data["user"] = get_user_details(lease_raw_data["user"])
+        lease_data["user"] = get_user_details(lease_raw_data["user"])["UserName"]
         lease_data["lease_id"] = lease_id
+
+        expiry_date = datetime.fromisoformat(lease_raw_data["expires"])
+        expiry_date_formatted = expiry_date.strftime("%Y-%m-%d %H:%M:%S")
+        expiry_date_natural = humanize.naturaltime(
+            expiry_date.replace(tzinfo=timezone.utc),
+            when=datetime.now(timezone.utc)
+        )
+
+        lease_data["expires"] = f"{expiry_date_formatted} ({expiry_date_natural})"
 
         active_leases.append(lease_data)
 
