6bca43d0f20f4c9d5413e07f2735474ebbfa5be6
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-04 09:53:22 +0000

    remove interface

--- apps/interface/leases/utils/deserialise.py
+++
@@ -1,6 +0,0 @@
-from boto3.dynamodb.types import TypeDeserializer
-
-deserializer = TypeDeserializer()
-
-def deserialise(response):
-    return [{k: deserializer.deserialize(v) for k, v in item.items()} for item in response["Items"]][0]
