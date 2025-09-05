08c779986dae55607a4376489459e8336afe9ae0
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-05 21:40:24 +0000

    single quote

--- apps/interface/eslint.config.ts
+++ apps/interface/eslint.config.ts
@@ -1,19 +1,19 @@
-import js from "@eslint/js"
-import globals from "globals"
-import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 import stylistic from '@stylistic/eslint-plugin'
-import { defineConfig } from "eslint/config"
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js, '@stylistic': stylistic },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
-    settings: { react: { version: "detect" } },
+    settings: { react: { version: 'detect' } },
     rules: {
-      "react/react-in-jsx-scope": "off"
+      'react/react-in-jsx-scope': 'off'
     }
   },
   tseslint.configs.recommended,
@@ -21,19 +21,19 @@ export default defineConfig([
     ...pluginReact.configs.flat.recommended,
     rules: {
       ...pluginReact.configs.flat.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "semi": ["error", "never"],
+      'react/react-in-jsx-scope': 'off',
+      'semi': ['error', 'never'],
       '@stylistic/indent': ['error', 2],
       '@stylistic/comma-dangle': ['error', 'never'],
       '@stylistic/quotes': ['error', 'single'],
-      "@typescript-eslint/no-unused-vars": ["error", {
-        "args": "all",
-        "argsIgnorePattern": "^_",
-        "caughtErrors": "all",
-        "caughtErrorsIgnorePattern": "^_",
-        "destructuredArrayIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-        "ignoreRestSiblings": true
+      '@typescript-eslint/no-unused-vars': ['error', {
+        'args': 'all',
+        'argsIgnorePattern': '^_',
+        'caughtErrors': 'all',
+        'caughtErrorsIgnorePattern': '^_',
+        'destructuredArrayIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'ignoreRestSiblings': true
       }]
     }
   }
