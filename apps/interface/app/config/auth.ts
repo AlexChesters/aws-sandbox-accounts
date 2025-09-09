83f5c98cb2ce362e391194aef5f1e85ad0cdae91
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-09 11:58:03 +0100

    fix

--- apps/interface/app/config/auth.ts
+++ apps/interface/app/config/auth.ts
@@ -2,6 +2,7 @@ interface AuthConfig {
   authority: string,
   clientId: string,
   redirectUri: string,
+  logoutRedirectUri: string,
   responseType: string,
   scope: string
 }
@@ -10,6 +11,7 @@ export default {
   authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_t4m80oL3Y',
   clientId: '3sjic4vag51jg3734gfb2s6kki',
   redirectUri: 'http://localhost:5173/auth/callback',
+  logoutRedirectUri: 'http://localhost:5173/auth/logout',
   responseType: 'code',
   scope: 'email openid profile'
 } satisfies AuthConfig
