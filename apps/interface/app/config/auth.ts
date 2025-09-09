e518d2e01a8476dcc6d1c2a79ea85dd62728c36c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-09 18:16:21 +0100

    add config for other envs

--- apps/interface/app/config/auth.ts
+++ apps/interface/app/config/auth.ts
@@ -1,17 +1,42 @@
 interface AuthConfig {
-  authority: string,
-  clientId: string,
-  redirectUri: string,
-  logoutRedirectUri: string,
-  responseType: string,
-  scope: string
+  authority: string;
+  clientId: string;
+  redirectUri: string;
+  logoutRedirectUri: string;
+  responseType: string;
+  scope: string;
 }
 
-export default {
-  authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_t4m80oL3Y',
-  clientId: '3sjic4vag51jg3734gfb2s6kki',
-  redirectUri: 'http://localhost:5173/auth/callback',
-  logoutRedirectUri: 'http://localhost:5173/auth/logout',
-  responseType: 'code',
-  scope: 'email openid profile'
-} satisfies AuthConfig
+const configs: Record<string, AuthConfig> = {
+  'localhost': {
+    authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_t4m80oL3Y',
+    clientId: '3sjic4vag51jg3734gfb2s6kki',
+    redirectUri: 'http://localhost:5173/auth/callback',
+    logoutRedirectUri: 'http://localhost:5173/auth/logout',
+    responseType: 'code',
+    scope: 'email openid profile'
+  },
+  'test.sandbox.alexchesters.com': {
+    authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_t4m80oL3Y',
+    clientId: '3sjic4vag51jg3734gfb2s6kki',
+    redirectUri: 'http://test.sandbox.alexchesters.com/auth/callback',
+    logoutRedirectUri: 'http://test.sandbox.alexchesters.com/auth/logout',
+    responseType: 'code',
+    scope: 'email openid profile'
+  },
+  'sandbox.alexchesters.com': {
+    authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_bQT90t9tf',
+    clientId: '3sjic4vag51jg3734gfb2s6kki',
+    redirectUri: 'http://sandbox.alexchesters.com/auth/callback',
+    logoutRedirectUri: 'http://sandbox.alexchesters.com/auth/logout',
+    responseType: 'code',
+    scope: 'email openid profile'
+  }
+}
+
+function getAuthConfig(): AuthConfig {
+  const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost'
+  return configs[hostname] || configs['sandbox.alexchesters.com']
+}
+
+export default getAuthConfig()
