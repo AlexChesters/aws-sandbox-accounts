49d7a72832037e10977334451dc7be1cef7cce64
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-11 11:32:42 +0100

    wip use of api

--- apps/interface/app/routes/home.tsx
+++ apps/interface/app/routes/home.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from 'react'
+import { useAuth } from 'react-oidc-context'
+
 import type { Route } from './+types/home'
+import { APIClient } from '~/services/aws-sandbox-accounts-api'
 
 export function meta(_: Route.MetaArgs) {
   return [
@@ -7,6 +11,22 @@ export function meta(_: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const auth = useAuth()
+
+  useEffect(() => {
+    if (auth.isAuthenticated && auth.user) {
+      const apiClient = new APIClient(auth.user?.access_token)
+
+      apiClient.helloWorld()
+        .then(data => {
+          console.log('API Response:', data)
+        })
+        .catch(error => {
+          console.error('API Error:', error)
+        })
+    }
+  }, [auth])
+
   return (
     <main>
       <h1>Hello, world!</h1>
