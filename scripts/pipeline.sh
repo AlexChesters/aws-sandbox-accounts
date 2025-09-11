1ea93134b692e0ed1f2cb26885210eb488253e95
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-11 11:24:40 +0100

    rename

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,7 +2,7 @@ set -e
 
 rain deploy \
   ci/codepipeline.yml \
-  codepipeline-aws-sandbox-accounts \
+  codepipeline-aws-sandbox-accounts-apps \
   --yes \
   --region eu-west-1 \
   --profile sandbox-administrator
