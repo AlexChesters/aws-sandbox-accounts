a630b1afa3132c23a6e3868de3d8494f9ec5aade
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-12 11:14:04 +0100

    display

--- apps/interface/app/routes/accounts/components/accounts-section/index.tsx
+++ apps/interface/app/routes/accounts/components/accounts-section/index.tsx
@@ -8,10 +8,23 @@ type AccountsSectionProps = {
   accounts: Account[]
 }
 
-export default function AccountsSection({ status }: AccountsSectionProps) {
+export default function AccountsSection({ status, accounts }: AccountsSectionProps) {
   return (
     <Paper elevation={2} sx={{ p: 2, m: 2 }}>
       <Typography variant="h4" component="h1" className='capitalized'>{status}</Typography>
+      {
+        accounts.length === 0 && (
+          <Typography variant="body1">No {status} accounts in the pool.</Typography>
+        )
+      }
+      {
+        accounts.map((account) => (
+          <div key={account.accountId}>
+            <Typography variant="body1">Account ID: {account.accountId}</Typography>
+            <hr />
+          </div>
+        ))
+      }
     </Paper>
   )
 }
