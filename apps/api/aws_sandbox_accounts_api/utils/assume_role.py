74b723cb5ffa35cf5fc99d4a958092746a7c4c5c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-12 15:07:18 +0100

    add util

---
+++ apps/api/aws_sandbox_accounts_api/utils/assume_role.py
@@ -0,0 +1,20 @@
+import os
+
+import boto3
+
+function_name = os.environ["AWS_LAMBDA_FUNCTION_NAME"]
+
+def assume_role(role_arn: str):
+    sts = boto3.client("sts")
+    assumed_role_object = sts.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName=function_name
+    )
+
+    credentials = assumed_role_object["Credentials"]
+
+    return boto3.Session(
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"]
+    )
