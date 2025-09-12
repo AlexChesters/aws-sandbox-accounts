4b2feba2950274fb478ddb7568063c726b95e991
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-12 11:32:45 +0100

    wip

--- apps/interface/app/routes/accounts/components/accounts-section/index.tsx
+++ apps/interface/app/routes/accounts/components/accounts-section/index.tsx
@@ -1,5 +1,8 @@
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 
 import { AccountStatus, type Account } from '~/models/types'
 
@@ -11,21 +14,24 @@ type AccountsSectionProps = {
 export default function AccountsSection({ status, accounts }: AccountsSectionProps) {
   return (
     <Paper elevation={2} sx={{ p: 2, m: 2 }}>
-      <Typography variant="h4" component="h1" className='capitalized'>{status}</Typography>
+      <Typography variant="h4" component="h1" className='capitalized' sx={{ mb: 1.5 }}>{status}</Typography>
       {
         accounts.length === 0 && (
           <Typography variant="body1">No {status} accounts in the pool.</Typography>
         )
       }
-      {
-        accounts.map((account) => (
-          <div key={account.accountId}>
-            <Typography variant="body1">Account ID: {account.accountId}</Typography>
-            <Typography variant="body1">Name: {account.name}</Typography>
-            <hr />
-          </div>
-        ))
-      }
+      <Grid container spacing={2}>
+        {
+          accounts.map((account) => (
+            <Card variant="outlined" key={account.accountId}>
+              <CardContent>
+                <Typography variant="h5" component="h2">{account.name}</Typography>
+                <Typography gutterBottom sx={{ color: 'text.secondary' }}>{account.accountId}</Typography>
+              </CardContent>
+            </Card>
+          ))
+        }
+      </Grid>
     </Paper>
   )
 }
