2b147f52c50048e0b320712338239916f2cc20b8
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-12 09:35:43 +0100

    fetch

--- apps/api/aws_sandbox_accounts_api/services/accounts.py
+++ apps/api/aws_sandbox_accounts_api/services/accounts.py
@@ -1,7 +1,11 @@
 from aws_sandbox_accounts_api.models.account import Account, AccountStatus
+from aws_sandbox_accounts_api.database_client import get_item
 
 def get_all_accounts() -> list[Account]:
-    for status in AccountStatus:
-        print(f"fetching accounts with status: {status.value}")
+    results = []
 
-    return []
+    for status in AccountStatus:
+        accounts = get_item(f"account_status#{status.value}")
+        results.extend(accounts)
+
+    return results
