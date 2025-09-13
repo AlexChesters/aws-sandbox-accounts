c3aecec5722a3fc7102e2dc394abb3d417579929
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-09-13 16:20:21 +0100

    add lease request api

--- apps/api/aws_sandbox_accounts_api/routers/leases.py
+++ apps/api/aws_sandbox_accounts_api/routers/leases.py
@@ -1,16 +1,32 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler.api_gateway import Router
 from aws_lambda_powertools.event_handler import Response, content_types
+from pydantic import BaseModel, ValidationError
+from aws_lambda_powertools.utilities.parser import parse
 
 from aws_sandbox_accounts_api.services import leases as leases_service
 
 logger = Logger()
 router = Router()
 
+class LeaseRequest(BaseModel):
+    user_id: str
+    duration: str
+
 @router.post("/")
 def create_lease():
-    leases_service.create_lease()
+    try:
+        lease_request = parse(event=router.current_event.json_body, model=LeaseRequest)
+    except ValidationError as e:
+        logger.error(f"Validation error: {e}")
+        return Response(
+            status_code=400,
+            content_type=content_types.APPLICATION_JSON,
+            body={"error": "Invalid request", "details": e.errors()},
+        )
+
+    execution_arn = leases_service.create_lease(lease_request.user_id, lease_request.duration)
 
     return Response(
-        status_code=202, content_type=content_types.APPLICATION_JSON, body={}
+        status_code=202, content_type=content_types.APPLICATION_JSON, body={"execution_arn": execution_arn}
     )
--- apps/api/aws_sandbox_accounts_api/services/leases.py
+++ apps/api/aws_sandbox_accounts_api/services/leases.py
@@ -1,2 +1,21 @@
-def create_lease():
-    pass
+import os
+import json
+
+import boto3
+
+step_functions = boto3.Session(region_name="eu-west-1").client("stepfunctions")
+
+def create_lease(*, user_id: str, duration: str) -> str:
+    step_function_arn = os.getenv("LEASE_CREATOR_STEP_FUNCTION_ARN")
+
+    start_response = step_functions.start_execution(
+        stateMachineArn=step_function_arn,
+        input=json.dumps({
+            "user_id": user_id,
+            "duration": duration
+        })
+    )
+
+    execution_arn = start_response["executionArn"]
+
+    return execution_arn
