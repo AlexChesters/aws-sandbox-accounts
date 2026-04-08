2ae2e2ee87feddf7b7ee2e8ecaa917efea940f9d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2026-04-08 20:10:00 +0100

    rm

--- apps/interface/app/routes/accounts/accounts-actions-context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-type AccountsActionsContextType = {
-  onCreateLease: () => void
-}
-export const AccountsActionsContext = createContext<AccountsActionsContextType>({
-  onCreateLease: () => {}
-})
--- apps/interface/scripts/pipeline.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-rain deploy \
-  ci/codepipeline.yml \
-  codepipeline-aws-sandbox-accounts-interface \
-  --yes \
-  --region eu-west-1 \
-  --profile sandbox-administrator
